7cc42508822da78cb74bbbd38ededa8bcec10826
Nikola Nikolov <nikola@emerchantpay.com>
2013-12-15 01:17:12 +0200

    Fix legend.

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -1,12 +1,10 @@
 class GraphicsController < ApplicationController
   def index
-    categories_amounts = FinanceLmay.amount_on_each_categories
+    @categories_amounts = FinanceLmay.amount_on_each_categories
     @categories = []
     @amount = []
-    categories_amounts.each do |category_amount| 
+    @categories_amounts.each do |category_amount| 
       @categories << category_amount['name']
-      p category_amount['sum'].to_f
-      p FinanceLmay.sum_amounts.to_f
       @amount << category_amount['sum'].to_f/FinanceLmay.sum_amounts.to_f*100
     end
   end
