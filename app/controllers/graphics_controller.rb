fd9ea83ac65ff008eff4ca1877e7fe31c810ca1d
Nikola Nikolov <nikola@emerchantpay.com>
2013-12-15 01:36:04 +0200

    sorting directory by amount.

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -1,6 +1,7 @@
 class GraphicsController < ApplicationController
   def index
-    @categories_amounts = FinanceLmay.amount_on_each_categories
+    categories_amounts = FinanceLmay.amount_on_each_categories
+    @categories_amounts =  categories_amounts.sort_by { |category_amount| -category_amount['sum'].to_i }
     @categories = []
     @amount = []
     @categories_amounts.each do |category_amount| 
