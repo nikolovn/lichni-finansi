d256e87c8e5cbc103bfe18a740ec19b5b4c4b28a
Nikola Nikolov <nikola@nikola>
2014-03-04 23:24:34 +0200

    Add spec for edit field and remove field

--- spec/features/enter_transaction_spec.rb
+++ spec/features/enter_transaction_spec.rb
@@ -10,13 +10,46 @@ feature 'Enter transactions' do
     visit 'categories'
 
     expect(page).to have_content 'Food and drinks'
-    expect(page).to have_content 'Transaport and comunications'
+    expect(page).to have_content 'Transport and comunications'
   end
 
   scenario 'Add fields and Enter the new transaction' do
+
+    visit 'categoies'
+
+    click_on 'food_and_drink'
+
+    within("#food_and_drink") do
+      expect(page).to have_field?('count', type:text)
+      expect(page).to have_field?('date', type:date)
+      expect(page).to have_fielld?('amount', type:text)
+      expect(page).to have_field?('description', type:text)
+
+      click_on 'add'
+
+      expect('page').to have_content 'Successfully added new transaction'
   end
-  
-  scenario 'Remove fields when do not want to enter a transaction in this directory'
+
+  scenario 'Remove fields when do not want to enter a transaction in this directory' do
+    visit 'categories'
+
+    click_on 'food_and_drink'
+
+    within("#food_and_drink") do
+      expect(page).to have_field?('count', type: text)
+      expect(page).to have_field?('date', type: date)
+      expect(page).to have_field?('amount', tupe: text)
+      expect(page).to have_field?('description', type: text)
+    end
+
+    click_on 'cancel'
+
+    within("#food_and_drink") do
+      expect(page).not_to have_field?('count', type: text)
+      expect(page).not_to have_field?('date', type: date)
+      expect(page).not_to have_field?('amount', type: text)
+      expect(page).not_to have_field?('description', type: text)
+    end
   end
 
   scenario 'Edit only last transaction if input with error' do
