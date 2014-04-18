8e341c9b638ec1acfdd7b24da42832d08c716b99
Nikola Nikolov <nikola@emerchantpay.com>
2014-04-18 22:18:30 +0300

    Add filter

--- app/controllers/all_transactions_controller.rb
+++ app/controllers/all_transactions_controller.rb
@@ -2,8 +2,16 @@ class AllTransactionsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @income_transactions = IncomeTransaction.all
-    @expense_transactions = ExpenseTransaction.all
+    @q_income_transactions = IncomeTransaction.search(params[:q])
+    @income_transactions = @q_income_transactions.result(distinct: true)
+
+    @q_expense_transactions = ExpenseTransaction.search(params[:q])
+    @expense_transactions = @q_expense_transactions.result(distinct: true)
+
+    @q_income_categories = IncomeCategory.search(params[:q])
+    @income_categories = @q_income_categories.result(distinct: true)
+
+    
 
   end
 end
