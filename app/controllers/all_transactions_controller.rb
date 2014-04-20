48cba2fee73656857588b13dcbd8d39ab6e5fa5e
Nikola Nikolov <nikola@emerchantpay.com>
2014-04-20 09:57:42 +0300

    tab for filters

--- app/controllers/all_transactions_controller.rb
+++ app/controllers/all_transactions_controller.rb
@@ -11,6 +11,9 @@ class AllTransactionsController < ApplicationController
     @q_income_categories = IncomeCategory.search(params[:q])
     @income_categories = @q_income_categories.result(distinct: true)
 
+    @q_expense_categories = ExpenseCategory.search(params[:q])
+    @expense_categories = @q_expense_categories.result(distinct: true)
+
     
 
   end
