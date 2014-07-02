6d6e7c2ac2d61d51f32e110d910dbb8b2fe30d4e
Nikola Nikolov <nikola@emerchantpay.com>
2014-07-02 11:25:56 +0300

    Add balance

--- app/controllers/all_transactions_controller.rb
+++ app/controllers/all_transactions_controller.rb
@@ -14,7 +14,12 @@ class AllTransactionsController < ApplicationController
     @q_expense_categories = current_user.expense_category.search(params[:q])
     @expense_categories = @q_expense_categories.result(distinct: true)
 
-    
+    amount = 0
+    income_transactions_amount = @income_transactions.each {|transaction| amount+= transaction.amount} 
+    income_transactions_amount = amount
+    expense_transactions_amount = @expense_transactions.each {|transaction| amount+= transaction.amount} 
+    expense_transactions_amount = amount
+    @current_balance = income_transactions_amount - expense_transactions_amount
 
   end
 end
