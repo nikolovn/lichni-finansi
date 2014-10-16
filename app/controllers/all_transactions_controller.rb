55bfcaa662aa17d5326a3b5afd8378a1354be9d7
Nikola Nikolov <nikola@emerchantpay.com>
2014-10-16 12:37:04 +0300

    fix balance

--- app/controllers/all_transactions_controller.rb
+++ app/controllers/all_transactions_controller.rb
@@ -19,25 +19,14 @@ class AllTransactionsController < ApplicationController
     @income_transactions = @q_income_transactions.result(distinct: true)
     @q_expense_transactions = current_user.expense_transactions.search(expense_params)
     @expense_transactions = @q_expense_transactions.result(distinct: true).order('date ASC')
-
-    # @q_income_categories = current_user.income_category.search(params[:q])
-    # @income_categories = @q_income_categories.result(distinct: true)
-
-    # @q_expense_categories = current_user.expense_category.search(params[:q])
-    # @expense_categories = @q_expense_categories.result(distinct: true)
-
-    @income_transactions_amount = 0
-    @expense_transactions_amount = 0
-    @income_transactions.each { |transaction| @income_transactions_amount+= transaction.amount }
-    #@expense_transactions.each { |transaction| @expense_transactions_amount+= transaction.amount} 
-    @current_balance = @income_transactions_amount - @expense_transactions_amount
+ 
+    @current_balance = @income_transactions.sum(:amount) - @expense_transactions.sum(:amount)
 
     @income_transactions_amount
     @expense_transactions_amount
 
     active_income
     active_expense
-
   end
 
   def start_date
