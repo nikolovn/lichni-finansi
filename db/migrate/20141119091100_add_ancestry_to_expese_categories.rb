ff4197ff3bde9b335acd274eaabd782ef8dfa389
Nikola Nikolov <nikola@emerchantpay.com>
2014-11-19 11:14:35 +0200

    Change gem for expense category

--- db/migrate/20141119091100_add_ancestry_to_expese_categories.rb
+++ db/migrate/20141119091100_add_ancestry_to_expese_categories.rb
@@ -1,7 +1,7 @@
 class AddAncestryToExpeseCategories < ActiveRecord::Migration
   def change
-    add_column :expese_categories, :ancestry, :string
-    add_column :expese_categories, :ancestry_depth, :integer, default: 0
-    add_index :expese_categories, :ancestry
+    add_column :expense_categories, :ancestry, :string
+    add_column :expense_categories, :ancestry_depth, :integer, default: 0
+    add_index :expense_categories, :ancestry
   end
 end
