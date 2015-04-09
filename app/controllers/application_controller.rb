3d5c7703da3622e7fab67c6f82e21a71899b4267
Nikola Nikolov <nikola@emerchantpay.com>
2015-04-09 16:39:02 +0300

    Add cooclies to кееп language settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
  
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    cookies[:locale] = params[:locale] if params[:locale]
+    I18n.locale = cookies[:locale] || I18n.default_locale
   end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
