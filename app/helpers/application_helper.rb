92a37ac6eb4fac450c7ff16ea2c286fceeef9ee5
Nikola Nikolov <nikola@emerchantpay.com>
2016-06-06 16:04:08 +0300

    Remove none from select field

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,6 @@ module ApplicationHelper
     expense_categories.each { |c| c.ancestry = c.ancestry.to_s + (c.ancestry != nil ? "/" : '') + c.id.to_s 
       }.sort {|x,y| x.ancestry <=> y.ancestry 
       }.map{ |c| ["-" * (c.depth - 1) + c.name,c.id] 
-      }.unshift(["-- none --", nil])
+      }
   end
 end
