adf601f6354e63ec3f386f0240d126432e281cb9
Nikola Nikolov <nikola@emerchantpay.com>
2016-06-07 13:09:00 +0300

    Add filter for period

--- lib/shared_params.rb
+++ lib/shared_params.rb
@@ -4,26 +4,29 @@ module SharedParams
       income_category_id_eq: params[:income_category_id],
       income_transaction_id_eq: params[:income_transaction_id],
       expense_category_id_in: select_expense_subcategory(params),
-      date_gteq: params[:from_date],
-      date_lteq: params[:to_date],
       description_cont: params[:description],
       expense_type_cont: params[:expense_type]
-    }
+    }.merge(date_params(params))
   end
 
   def self.income_params(params)
     {
       id_eq: params[:income_transaction_id],
       income_category_id_eq: params[:income_category_id],
-      date_gteq: params[:from_date],
-      date_lteq: params[:to_date],
       description_cont: params[:description]
-    }
+    }.merge(date_params(params))
   end
 
   def self.select_expense_subcategory(params)
     if params[:expense_category_id].present?
       ExpenseCategory.find(params[:expense_category_id]).subtree_ids
     end
+  end
+
+  def self.date_params(params)
+    {
+      date_gteq: params[:from_date] || Date.today,
+      date_lteq: params[:to_date] || Date.today - 1.year
+    }
   end
 end
