a77905201910e79bfb59bc7f559c5f7b70d45cbc
Nikola Nikolov <nikola@emerchantpay.com>
2016-06-07 13:56:06 +0300

    Change date params

--- lib/shared_params.rb
+++ lib/shared_params.rb
@@ -5,28 +5,25 @@ module SharedParams
       income_transaction_id_eq: params[:income_transaction_id],
       expense_category_id_in: select_expense_subcategory(params),
       description_cont: params[:description],
-      expense_type_cont: params[:expense_type]
-    }.merge(date_params(params))
+      expense_type_cont: params[:expense_type],
+      date_gteq: params[:from_date],
+      date_lteq: params[:to_date]
+    }
   end
 
   def self.income_params(params)
     {
       id_eq: params[:income_transaction_id],
       income_category_id_eq: params[:income_category_id],
-      description_cont: params[:description]
-    }.merge(date_params(params))
+      description_cont: params[:description],
+      date_gteq: params[:from_date],
+      date_lteq: params[:to_date]
+    }
   end
 
   def self.select_expense_subcategory(params)
     if params[:expense_category_id].present?
       ExpenseCategory.find(params[:expense_category_id]).subtree_ids
     end
-  end
-
-  def self.date_params(params)
-    {
-      date_gteq: params[:from_date] || (Date.today -1.year).strftime("%d/%m/%Y"),
-      date_lteq: params[:to_date] || Date.today.strftime("%d/%m/%Y")
-    }
   end
 end
